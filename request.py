4c85c7d5ba6fe30d7d9cdb54651ed52eabd30adf
soumi7 <soumibardhan10@gmail.com>
2020-05-20 13:00:19 +0530

    changes

--- request.py
+++ request.py
@@ -1,6 +1,6 @@
 import requests
 
 url = 'http://localhost:5000/predict_api'
-r = requests.post(url,json={'experience':2, 'test_score':9, 'interview_score':6})
+r = requests.post(url,json={'Date':2, 'Product name':9, 'Product Group':6})
 
 print(r.json())
