99b73f2e0669f0e34d4228e8d67764c6340dd5a2
soumi7 <soumibardhan10@gmail.com>
2020-05-20 15:39:02 +0530

    chnged app

--- app.py
+++ app.py
@@ -17,13 +17,13 @@ def predict():
     int_features = [int(x) for x in request.form.values()]
     print(int_features)
     date = int_features[0]
-    
+
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Quantity of product predicted is $ {}'.format(output))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
