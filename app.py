25c623e9b31b7099dc1020fa8b0558011311af81
soumi7 <soumibardhan10@gmail.com>
2020-05-23 23:33:57 +0530

    changes to app

--- app.py
+++ app.py
@@ -64,19 +64,19 @@ def predict():
 
 
 
-    #frequency = 24
-    #start_date = start_date
-    #end_date = end_date
-    #api_key = '12b2c18a34194a8ca93113127200405'
-    #location_list = ['Mlawa']
-    #hist_weather_data = retrieve_hist_data(api_key,
-                                            #location_list,
-                                            #start_date,
-                                            #end_date,
-                                            #frequency,
-                                            #location_label = False,
-                                            #export_csv = True,
-                                            #store_df = True)
+    frequency = 24
+    start_date = start_date
+    end_date = end_date
+    api_key = '12b2c18a34194a8ca93113127200405'
+    location_list = ['Mlawa']
+    hist_weather_data = retrieve_hist_data(api_key,
+                                            location_list,
+                                            start_date,
+                                            end_date,
+                                            frequency,
+                                            location_label = False,
+                                            export_csv = True,
+                                            store_df = True)
 
     
     monthly_weather_data=pd.read_csv("Month_Weather_data.csv")
