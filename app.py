776a62cb66ddaf5d1347622205aa0f56a036cdab
soumi7 <soumibardhan10@gmail.com>
2020-05-23 19:13:25 +0530

    added Datasets and updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
+from datetime import datetime , timedelta
+import pandas as pd
+import pycaret
+import requests
+from wwo_hist import retrieve_hist_data
 
 app = Flask(__name__)
 model = pickle.load(open('model.pkl', 'rb'))
@@ -23,7 +28,7 @@ def predict():
 
     output = round(prediction[0], 2)
 
-    return render_template('index.html', prediction_text='Quantity of product predicted is $ {}'.format(output))
+    return render_template('index.html', prediction_text='Quantity of product predicted is  {}'.format(output))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
@@ -31,10 +36,135 @@ def predict_api():
     For direct API calls trought request
     '''
     data = request.get_json(force=True)
-    prediction = model.predict([np.array(list(data.values()))])
+    
+    date=data.Date #enter the month and year for which you want to predict the sales of the product [EXAMPLE : 02-2020 (FEB 2020)]: "
 
-    output = prediction[0]
+    converted_datetime=pd.to_datetime(date).date()
+
+    def dateaddition (converted_datetime):
+        if int(str(converted_datetime).split("-")[0])%4 ==0:
+            if (int(str(converted_datetime).split("-")[1])) ==2:
+                added_days=29
+            elif int(str(converted_datetime).split("-")[1]) %2 ==0 and int(str(converted_datetime).split("-")[1]) <=6:   
+                added_days=30
+            elif int(str(converted_datetime).split("-")[1]) %2 ==1 and int(str(converted_datetime).split("-")[1]) <=6:
+                added_days=31
+            elif  int(str(converted_datetime).split("-")[1]) %2 ==0 and int(str(converted_datetime).split("-")[1]) >=7:
+                added_days=31
+            elif int(str(converted_datetime).split("-")[1]) %2 ==1 and int(str(converted_datetime).split("-")[1]) >=7:
+                added_days=30  
+        elif int(str(converted_datetime).split("-")[0])%4 !=0:
+            if (int(str(converted_datetime).split("-")[1])) ==2:
+                added_days=28
+            elif int(str(converted_datetime).split("-")[1]) %2 ==0 and int(str(converted_datetime).split("-")[1]) <=6:   
+                added_days=30
+            elif int(str(converted_datetime).split("-")[1]) %2 ==1 and int(str(converted_datetime).split("-")[1]) <=6:
+                added_days=31
+            elif  int(str(converted_datetime).split("-")[1]) %2 ==0 and int(str(converted_datetime).split("-")[1]) >=7:
+                added_days=31
+            elif int(str(converted_datetime).split("-")[1]) %2 ==1 and int(str(converted_datetime).split("-")[1]) >=7:
+                added_days=30  
+
+        return added_days
+
+
+    end_date= pd.to_datetime(converted_datetime+timedelta(dateaddition(converted_datetime)-1)).date().strftime("%d-%b-%Y")
+
+
+    start_date = converted_datetime.strftime("%d-%b-%Y")
+
+    print(start_date,"   ",end_date)
+
+
+
+
+
+    frequency = 24
+    start_date = start_date
+    end_date = end_date
+    api_key = '12b2c18a34194a8ca93113127200405'
+    location_list = ['Mlawa']
+    hist_weather_data = retrieve_hist_data(api_key,
+                                            location_list,
+                                            start_date,
+                                            end_date,
+                                            frequency,
+                                            location_label = False,
+                                            export_csv = True,
+                                            store_df = True)
+
+    
+    monthly_weather_data=pd.read_csv("Month_Weather_data.csv")
+
+    monthly_weather_data=monthly_weather_data.drop(["date_time","totalSnow_cm","sunHour","uvIndex.1","uvIndex","moon_illumination","moonrise","moonset","sunrise","DewPointC","sunset","WindChillC","WindGustKmph","precipMM","pressure","visibility","winddirDegree","windspeedKmph","tempC"],axis=1)
+
+
+    monthly_weather_data["avg_temp"]=(monthly_weather_data["maxtempC"]+monthly_weather_data["mintempC"])/2
+
+    monthly_weather_data=monthly_weather_data.drop(["maxtempC","mintempC"],axis=1)
+
+    monthly_weather_data=monthly_weather_data[["avg_temp","FeelsLikeC","HeatIndexC","cloudcover","humidity",]]
+    # monthly_weather_data=monthly_weather_data[["avg_temp","FeelsLikeC","HeatIndexC","cloudcover","humidity","ishol/week"]]
+
+    # print(monthly_weather_data.head())
+
+    def cat_heat(heatindex):
+        # monthly_heatindex=[]
+        # for heatindex in data["HeatIndexC"]:
+        if heatindex < -2:
+            return (0)
+        elif heatindex >=-1 and heatindex<=14:
+            return (1)
+        else:
+            return (2)
+        
+
+
+
+    def cat_cloud(cloudcover):
+        # monthly_cloudcover=[]
+        # for cloudcover in data["cloudcover"]:
+        if cloudcover < 25:
+            return(0)
+        elif cloudcover >=25 and cloudcover<50:
+            return(1)
+        elif cloudcover >=50 and cloudcover<75:
+            return(1)    
+        else:
+            return(3)
+        
+            
+
+            
+
+    def mean_data(data):
+        values=[]
+        for key,value in data.iteritems():
+            values.append(value.mean())
+            print(key)
+            return values
+
+    monthly_averages=np.around(mean_data(monthly_weather_data),2)
+    monthly_averages[2]=cat_heat(monthly_averages[2])
+    monthly_averages[3]=cat_cloud(monthly_averages[3])
+        
+    print(monthly_averages)
+
+    from pycaret.regression import*
+
+
+    loaded_model=load_model("Final_Mod")
+    pred=Model.predict([[ 2.52 ,-0.38 , 1.18 , 1.06,  1.01, -0.25 ,-0.34]])
+
+    unseen_data=pd.read_csv("GROUP_OF_DATASETS/SWEETS.csv")
+
+    prediction=predict_model(loaded_model, data= unseen_data.head(5))
+
+    output = prediction['Label']
     return jsonify(output)
+
+
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
